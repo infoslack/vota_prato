6487821fd06e79bc89d28f63ddf36124c11596e2
Daniel Romero <infoslack@gmail.com>
2011-03-13 05:39:15 -0300

    Criado aplicativo rails para votação de pratos em restaurantes conhecidos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VotaPrato::Application.config.secret_token = '9bdd9318ea94a18b1d6812dad1800835bfc2eeba7158d259e55cf2c5fa3bc067ef9d10870dacf46ac18023bfe47f498dd72ce05b927cdb5e8566a7e44ecd901e'
